c9e4b7a6e4658d9f679f75b6032bd5b65981a5d9
wpcc <310624463@qq.com>
2019-03-09 10:29:38 +0800

    modification

--- v-admin/src/views/nested/menu1/menu1-1/index.vue
+++
@@ -1,3 +0,0 @@
-<template lang="html">
-  <h3>menu1-1 page</h3>
-</template>
--- v-admin/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  lintOnSave: false,
-  devServer:{
-    open:true,
-    port:9527
-  },
-  baseUrl:'./'
-}
